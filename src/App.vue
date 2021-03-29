371eccd7f743b8e07c5821e3f9d58173fa6e936c
junhochoi <braverokmc79@gmail.com>
2021-03-29 23:39:13 +0900

    Header.vue 추가

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <template>
   <div id="app">
+    <Header />
     <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
 
 <script>
+import Header from '@/components/Header'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components:{
+    Header,
+  }
+
 }
 </script>
 
@@ -18,6 +25,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
